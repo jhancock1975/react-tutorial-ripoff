09b12cff74e09079ec371715a9215a762c331976
john <jhancock1975@gmail.com>
2016-09-11 12:24:04 -0400

    finish with poor implementation of post to comments

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ var express = require("express");
 var app = express();
 var path=require('path');
 var router = express.Router();
+var parser=require('body-parser');
+const fs = require('fs');
+
+app.use(parser.urlencoded({extended : true}));
+
 router.get('/comments', function(req, res){
   res.sendFile(path.join(__dirname, '/comments.json'));
+});
+
+router.post('/comments', function(req, res){
+  var comments=require(path.join(__dirname, '/comments.json'));
+  console.log('req.body = ', req.body, JSON.stringify(req.body));
+  comments.push(req.body);
+  fs.writeFile('./comments.json', JSON.stringify(comments));
+  console.log(JSON.stringify(comments));
+  res.send(JSON.stringify(comments));
 });
 app.use('/api', router);
 
