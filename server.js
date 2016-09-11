8f5bc580c471b239e2eaa5e30560ef9a07998ef4
john <jhancock1975@gmail.com>
2016-09-10 20:48:27 -0400

    fix spelling of sendFile, added dev dependencies via install of browserify and babelify packages, included browserified browser.js from babel-core

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 app = express();
 path=require('path');
 app.get('/', function(req, res){
-  res.sendfile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
@@ -13,7 +13,7 @@ console.log('__dirname = ', __dirname);
 /* serves all the static files */
 app.get(/^(.+)$/, function(req, res){
  console.log('static file request : ' + req.params);
-   res.sendfile( __dirname + req.params[0]);
+   res.sendFile( __dirname + req.params[0]);
 });
 //test
 var port = process.env.PORT || 5000;
